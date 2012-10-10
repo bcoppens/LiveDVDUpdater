80586631771c42d83d8d5f46083289a70ab53dc7
Bart Coppens <kde@bartcoppens.be>
2012-10-10 19:13:44 +0200

    Start firefox pointing to github when they should add the public key

--- create_keypair.sh
+++ create_keypair.sh
@@ -5,8 +5,11 @@ ssh-keygen -C "$USER For the Live DVD" -f ~/.ssh/${USER}_rsa
 echo "Plak dit in je browser!"
 cat ~/.ssh/${USER}_rsa.pub
 
+firefox https://github.ugent.be/settings/ssh&
+
 # Actually use this key, too
-echo 'IdentityFile ~/.ssh/'${USER}_rsa.pub >> ~/.ssh/config
+echo 'IdentityFile ~/.ssh/'${USER}_rsa >> ~/.ssh/config
 chmod o-rw ~/.ssh/config
 chmod g-rw ~/.ssh/config
+
 
