48c97dada2a95870ab28eac1fe400b92cbbf49f6
Bart Coppens <kde@bartcoppens.be>
2012-10-10 18:53:14 +0200

    Add the key file to the list of key pairs to look in

--- create_keypair.sh
+++ create_keypair.sh
@@ -5,3 +5,8 @@ ssh-keygen -C "$USER For the Live DVD" -f ~/.ssh/${USER}_rsa
 echo "Plak dit in je browser!"
 cat ~/.ssh/${USER}_rsa.pub
 
+# Actually use this key, too
+echo 'IdentityFile ~/.ssh/'${USER}_rsa.pub >> ~/.ssh/config
+chmod o-rw ~/.ssh/config
+chmod g-rw ~/.ssh/config
+
